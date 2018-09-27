a24838b05b6efb62aa509cbdc153e376438c652c
prajkum <42783099+prajkum@users.noreply.github.com>
2018-09-26 23:00:12 -0400

    Update StringUtils.java

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -2,12 +2,12 @@ public class StringUtils {
     /**
      * makePurdueUsername -- <= 8-letter-long lowercase username creator
      * Usernames may be AT MOST 8 letters in length, and should
-     * consist of the first letter of one's first name, and the 
+     * consist of the first letter of one's first name, and the
      * amount of their last name that will fit into the remaining
      * 7 characters. All should be lowercase.
      * Ex: a = "Sean ", b = "Flannery", returns "sflanner"
      */
-    public static String makePurdueUsername(String firstName, String lastName) {
+     static String makePurdueUsername(String firstName, String lastName) {
         /* Generate the long version of the name first:
             Ex: firstName = "Sean", lastName = "Flannery"
                     --> res = "SFlannery"
@@ -17,12 +17,12 @@ public class StringUtils {
             Ex: res = "SFlannery"
                     --> res = "SFlanner"
          */
-        res.substring(0, 8);
+        res= res +res.substring(0,8);
         /* Convert the name to lower-case
             Ex: res = "SFlanner"
                     --> res = "sflanner"
          */
-        res.toLowerCase();
+        res= res.toLowerCase();
         /* return the result of our calculation */
         return res;
     }
@@ -30,7 +30,7 @@ public class StringUtils {
     /**
      * replaceStudentUsername -- replace student usernames in sensitive text
      * We want to make sure student information is expunged from sensitive
-     * documents as much as possible. 
+     * documents as much as possible.
      * Ex: text = "jframes was among the students whose SSNs were leaked.",
      * username = "[DATA EXPUNGED] was among the students whose SSNs were leaked.",
      */
