0d52b885e78be711936f1c136f52914d15030999
prajkum <42783099+prajkum@users.noreply.github.com>
2018-09-27 00:10:13 -0400

    Update MathUtils.java

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -3,11 +3,12 @@ public class MathUtils {
      * divideNumbers -- a / b
      * Divide a by b and return the result
      */
-    public static double divideNumbers(int dividend, int divisor) {
+    static double divideNumbers(int dividend, int divisor) {
         /* Do some casual division ... Super easy ... Shouldn't be any problems here */
-        double res = dividend / divisor;
+        double res1;
+        res1= (double) dividend / divisor;
         /* Return the DEFINITELY CORRECT result */
-        return res;
+        return res1;
     }
 
     /**
@@ -29,17 +30,18 @@ public class MathUtils {
             Ex: base = 2, exp = 2
                --> res = 2, then res = 4 */
         for (int i = 0; i < exp; i++) {
-           res *= base; 
-        }
+            res *= base;
+        } 
 
         /* If it's a negative exponent, we should invert it! */
         if (isNegative) {
-            double dividedRes = divideNumbers(1, res);
-            return dividedRes;
+            double dividedres;
+            dividedres= divideNumbers(1, res);
+            return dividedres;
         }
         /* Otherwise, we are safe to just return the result */
         else {
             return res;
         }
-    }    
+    }
 }
